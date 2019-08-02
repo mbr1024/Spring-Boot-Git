3eb2f3c100d851e1a4c7d3137ba72bab4d321fe0
Su Yilin <964498287@qq.com>
2019-08-02 22:07:50 +0800

    我的第一个Spring Boot +git

---
+++ src/main/java/com/ujiuye/learnin_git/LearninGitApplication.java
@@ -0,0 +1,13 @@
+package com.ujiuye.learnin_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearninGitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ujiuye/learnin_git/handler/TestController.java
@@ -0,0 +1,18 @@
+package com.ujiuye.learnin_git.handler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * company: www.abc.com
+ * Author: Administrator
+ * Create Data: 2019/8/2
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/index")
+    public String index(){
+        return "hello Git";
+    }
+}
